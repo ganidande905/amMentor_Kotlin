21b2d37285eeb96f53696d16fdd923e4af85715e
ganidande905 <ganidande9@gmail.com>
2025-03-08 16:29:33 +0530

    Added the theme file

---
+++ app/src/main/java/com/example/ammentor/data/Theme.kt
@@ -0,0 +1,19 @@
+package com.example.ammentor.data
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+private val DarkColorScheme = darkColorScheme(
+    primary = Color(0xFFFFD700),
+    background = Color.Black,
+    onBackground = Color.White
+)
+
+@Composable
+fun MentorTrackerTheme(content: @Composable () -> Unit) {
+    MaterialTheme(
+        colorScheme = DarkColorScheme,
+        typography = Typography(),
+        content = content
+    )
+}
